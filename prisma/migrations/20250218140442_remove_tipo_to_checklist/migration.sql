6a774438e001b085eb008b2b157a8909fc3f5298
adryel martins <adryeldev@hotmail.com>
2025-02-18 18:39:21 -0300

    deploy atualizando checklist

---
+++ prisma/migrations/20250218140442_remove_tipo_to_checklist/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tipo` on the `checklist` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `checklist` DROP COLUMN `tipo`;
