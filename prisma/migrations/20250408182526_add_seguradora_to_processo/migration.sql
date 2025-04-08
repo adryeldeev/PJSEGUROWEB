af108ffd90d2c16b7ab5629369ad5b572abf77cc
adryel martins <adryeldev@hotmail.com>
2025-04-08 18:52:38 -0300

    deploy att schema.prisma

--- prisma/migrations/20250408182526_add_seguradora_to_processo/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `processo` ADD COLUMN `seguradoraId` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `Processo` ADD CONSTRAINT `Processo_seguradoraId_fkey` FOREIGN KEY (`seguradoraId`) REFERENCES `Seguradora`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20250408184825_add_ano_to_tipo_de_veiculo/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `nome` on the `tipodeveiculo` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `tipodeveiculo` DROP COLUMN `nome`,
-    ADD COLUMN `ano` INTEGER NULL;
